947a06ddb91c875cd4442c471215e14ebce6da97
ViharevN <swix_off@mail.ru>
2023-03-13 00:33:55 +0300

    commit

---
+++ src/test/java/me/viharev/hibernate/HibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package me.viharev.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
